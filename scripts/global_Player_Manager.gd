292603e20fdde236de4a2c7c2eda5f4b9b82d52f
Martim-F <martimtrex@hotmail.com>
2025-10-16 17:18:49 +0100

    fixed menu to level

--- scripts/global_Player_Manager.gd
+++ scripts/global_Player_Manager.gd
@@ -3,10 +3,6 @@ const PLAYER =preload("res://scenes/Player/Player.tscn")
 
 var player:Player
 
-func _ready() -> void:
-	add_player_instance()
-
-
 func add_player_instance()-> void:
 	player = PLAYER.instantiate()
 	add_child(player)
@@ -16,6 +12,8 @@ func unparent_player( _p :Node2D)->void:
 	_p.remove_child(player)
 	
 func set_as_parent( _p : Node2D) -> void:
+	if !player:
+		add_player_instance()
 	if player.get_parent():
 		player.get_parent().remove_child(player)
 	_p.add_child(player)
