b17e78d4903e06d1a47712b2cdee537cef35f29c
ist1106037 <goncalo.r.f.rocha@tecnico.ulisboa.pt>
2025-10-17 22:50:15 +0100

    pressure plate puzzle for corridor 2

--- scenes/Interactibles/pressure.gd
+++ scenes/Interactibles/pressure.gd
@@ -1,7 +1,4 @@
-extends Node
-
-var greg = preload("res://assets/greg_circ.png")
-var red = preload("res://assets/red_circ.png")
+extends Node2D
 
 var overlapping_bodies: Array[Node2D] = []
 
@@ -9,7 +6,6 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.name == "Player" || body.name == "Pushable":
 		if overlapping_bodies.is_empty():
 			EventManager.trigger_pressure_on()
-			$Sprite2D.texture = greg
 		overlapping_bodies.append(body)
 		
 func _on_area_2d_body_exited(body: Node2D) -> void:
@@ -17,4 +13,3 @@ func _on_area_2d_body_exited(body: Node2D) -> void:
 		overlapping_bodies.erase(body)
 		if overlapping_bodies.is_empty():
 			EventManager.trigger_pressure_off()
-			$Sprite2D.texture = red
--- scenes/doors/door_right.gd
+++ scenes/doors/door_right.gd
@@ -1,11 +1,18 @@
-extends Node2D
+extends StaticBody2D
 @onready var sprite = $Sprite2D
+@onready var collision : CollisionShape2D = $CollisionShape2D
 
 func _ready() -> void:
 	sprite.play("stop")
 	EventManager.puzzle_solved.connect(_on_door_open)
+	EventManager.pressure_on.connect(_on_door_open)
+	EventManager.pressure_off.connect(_on_door_close)
 	pass
 
 func _on_door_open() -> void:
 	sprite.play("moving")
-	queue_free()
+	collision.set_deferred("disabled", true)
+
+func _on_door_close() -> void:
+	sprite.play_backwards("moving")
+	collision.set_deferred("disabled", false)
