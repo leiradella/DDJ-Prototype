42c1ed2ed972ab8de4ddad2cb2d5acabdbc6ff61
itsahre <miguel1432gomes@gmail.com>
2025-10-19 16:09:50 +0100

    fixed pickups and implemented sprites

--- scenes/Items/pickup.gd
+++ scenes/Items/pickup.gd
@@ -1,24 +1,22 @@
 extends Node
 
-@onready var label = $Node2D/Label
-@onready var area = $Node2D/Area2D
-
+@onready var area = $Area2D
+@onready var sprite = $Sprite2D
 @export var type: ItemType
 @export var quantity: int
 @onready var item: Item
 
 enum ItemType {MEDKIT, BULLET}
 
-const ITEM_RESOURCES := {
+const ITEM_TYPE := {
 	ItemType.MEDKIT: preload("res://scenes/Items/medkit.tres"),
 	ItemType.BULLET: preload("res://scenes/Items/bullet_item.tres")
 }
 
 func _ready():
-	item = ITEM_RESOURCES.get(type)
-	label.text = str(item.name)
-	$Node2D.position = Vector2(randi() % 1920, randi() % 1080)
-
+	item = ITEM_TYPE.get(type)
+	sprite.texture = item.icon
+	
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		EventManager.trigger_item_pickedup(item, quantity)
