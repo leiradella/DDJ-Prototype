e12cf5b842b48503b82fc542aafffe51c409c0fc
Martim-F <martimtrex@hotmail.com>
2025-10-19 20:32:52 +0100

    added player position to maps

--- scenes/level_map/world_map_overlay.gd
+++ scenes/level_map/world_map_overlay.gd
@@ -1,16 +1,28 @@
 extends CanvasLayer
 
-@onready var map_image = $MapImage
+@onready var map_image: TextureRect = $MapImage
+@onready var player_marker: Node2D = $Sprite2D
+
+var current_room_pos: Vector2 = Vector2.ZERO
+
+func set_player_room(room_map_pos: Vector2) -> void:
+	current_room_pos = room_map_pos
+	update_player_marker()
 
 func show_map():
 	map_image.visible = true
+	player_marker.visible = true
+	update_player_marker()
 
 func hide_map():
 	map_image.visible = false
+	player_marker.visible = false
 
 func _input(event: InputEvent):
 	if event.is_action_pressed("show_map"):
 		show_map()
-		print("a")
 	elif event.is_action_released("show_map"):
 		hide_map()
+
+func update_player_marker():
+	player_marker.position = current_room_pos
