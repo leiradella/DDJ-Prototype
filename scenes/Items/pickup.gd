8dce1a806269fe9d2af7a5b61907832ed62c22f6
Martim-F <martimtrex@hotmail.com>
2025-10-19 18:55:01 +0100

    item state for all items done

--- scenes/Items/pickup.gd
+++ scenes/Items/pickup.gd
@@ -6,7 +6,7 @@ extends Node
 
 @export var type: ItemType
 @export var quantity: int
-
+@export var item_id : String = ""
 
 enum ItemType {MEDKIT, BULLET, FIRST_CLUE, SECOND_CLUE, THIRD_CLUE}
 
@@ -21,8 +21,18 @@ const ITEM_TYPE := {
 func _ready():
 	item = ITEM_TYPE.get(type)
 	sprite.texture = item.icon
+	var level_key = get_tree().current_scene.scene_file_path
+	var level_state = GameState.get_level_state(level_key)
+	if level_state and level_state.is_item_collected(item_id):
+		queue_free()
+		return
 	
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
+		var level_key = get_tree().current_scene.scene_file_path
+		var level_state = GameState.get_level_state(level_key)
+		if level_state:
+			level_state.mark_item_collected(item_id)
+			GlobalState.save()
 		EventManager.trigger_item_pickedup(item, quantity)
 		queue_free()
